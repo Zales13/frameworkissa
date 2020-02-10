7990f63cb52c71c53cadb880798e7e8d004c36e6
issa <issa@adminb2b.local>
2020-02-10 17:00:45 +0100

    admin et membre modif

--- routes/web.php
+++ routes/web.php
@@ -43,11 +43,13 @@ Route::post('delete', 'UsersController@delete');
 Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function()
 {
 Route::match(['get', 'post'], '/adminOnlyPage/', 'HomeController@admin');
+Route::get('/adminOnlyPage','UsersController@afficher')->name('current_users_admin');
 });
 
 
 Route::group(['middleware' => 'App\Http\Middleware\MemberMiddleware'], function()
 {
 Route::match(['get', 'post'], '/memberOnlyPage/', 'HomeController@member');
+Route::get('/memberOnlyPage','UsersController@afficher')->name('current_users_member');
 });
 
